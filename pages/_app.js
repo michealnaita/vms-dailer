8544d18790542f6bc31acce420fde6638abf6d0b
michealnaita <michealnaita@gmail.com>
2022-08-02 06:33:28 +0300

    adds google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}
+      ></Script>
+      <Script strategy="lazyOnload" id="">
+        {` 
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', '${process.env.MEASUREMENT_ID}');
+        `}
+      </Script>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
